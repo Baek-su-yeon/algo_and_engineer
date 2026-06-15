773b8a5e2f9ee7f5406c0606c51c80a76ff4604b
yunju <skadbsnwk@naver.com>
2026-06-15 17:27:47 +0900

    260615_남윤주_타겟넘버 풀이

---
+++ 알고리즘/260615/260615_남윤주_타겟넘버.java
@@ -0,0 +1,21 @@
+class Solution {
+    int answer = 0;
+
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
+        return answer;
+    }
+
+    private void dfs(int[] numbers, int target, int index, int sum) {
+        //배열 마지막 원소까지 모두 탐색 완료 시 종료
+        if (index == numbers.length) {
+            if (sum == target) {
+                answer++;
+            }
+            return;
+        }
+
+        dfs(numbers, target, index+1, sum + numbers[index]);
+        dfs(numbers, target, index+1, sum - numbers[index]);
+    }
+}
